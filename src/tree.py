11664f46984d9ec72d334c0eebc17e94d38ebb18
JHpusd <justinhongdebate@gmail.com>
2021-01-23 00:01:55 +0000

    refactored tree class to include index

--- src/tree.py
+++ src/tree.py
@@ -1,47 +1,47 @@
 class TreeNode():
-    def __init__(self, parent, value, children):
+    def __init__(self, parent, value, index, children):
         self.parent = parent
         self.value = value
+        self.index = index
         self.children = children
 
 class Tree():
-    def __init__(self, edges):
+    def __init__(self, edges, node_values):
         self.edges = edges
-        self.root = TreeNode(None, self.get_root(self.edges), None)
+        self.val_list = node_values
+        root_value = self.val_list[self.get_root(self.edges)]
+        root_index = self.get_root(self.edges)
+        self.root = TreeNode(None, root_value, root_index, None)
     
-    def get_children(self, target, tree):
+    def get_children(self, target_index, edges):
         result_list = []
-        for branch in tree:
-            if branch[0] == target:
+        for branch in edges:
+            if branch[0] == target_index:
                 result_list.append(branch[1])
         return result_list
 
-    def get_parents(self, target, tree):
-        for branch in tree:
-            if branch[1] == target:
+    def get_parent(self, target_index, edges):
+        for branch in edges:
+            if branch[1] == target_index:
                 return branch[0]
-            else:
-                continue
 
-    def get_root(self, tree):
-        for branch in tree:
-            if self.get_parents(branch[0], tree) == None:
+    def get_root(self, edges):
+        for branch in edges:
+            if self.get_parent(branch[0], edges) == None:
                 return branch[0]
-            else:
-                continue
 
     def build_from_edges(self):
         current_nodes = [self.root]
         while len(current_nodes) != 0:
             all_children = []
             for node in current_nodes:
-                node_children = self.get_children(node.value, self.edges)
+                node_children = self.get_children(node.index, self.edges)
                 for i in range(len(node_children)):
-                    node_children[i] = TreeNode(node, node_children[i], None)
+                    node_children[i] = TreeNode(node, self.val_list[node_children[i]], node_children[i], None)
                 node.children = node_children
                 for i in node_children:
                     all_children.append(i)
-            current_nodes = list(all_children)
+            current_nodes = all_children
     
     def nodes_breadth_first(self):
         queue = [self.root]
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,13 +1,13 @@
 import sys
 sys.path.append('src')
 from tree import *
-
+'''
 edges = [('a','c'), ('e','g'), ('e','i'), ('e','a'), ('d','b'), ('a','d'), ('d','f'), ('f','h'), ('d','j'), ('d','k')]
 
 tree = Tree(edges)
 
 tree.build_from_edges()
-'''
+
 print("Testing that Tree class works...")
 assert tree.root.value == 'e'
 assert [node.value for node in tree.root.children]==['g', 'i', 'a']
@@ -22,7 +22,7 @@ assert [node.value for node in tree.root.children[2].children[1].children[0].chi
 assert [node.value for node in tree.root.children[2].children[1].children[1].children]==[]
 assert [node.value for node in tree.root.children[2].children[1].children[0].children[0].children]==[]
 print("PASSED")
-'''
+
 print("Testing breadth and depth-first ")
 breadth_first_nodes = tree.nodes_breadth_first()
 assert [node.value for node in breadth_first_nodes] == [
@@ -32,3 +32,71 @@ depth_first_nodes = tree.nodes_depth_first()
 assert [node.value for node in depth_first_nodes] == [
     'e', 'a', 'd', 'k', 'j', 'f', 'h', 'b', 'c', 'i', 'g']
 print("PASSED")
+'''
+node_values = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+edges = [(0,2), (4,6), (4,8), (4,0), (3,1), (0,3), (3,5), (5,7), (3,9), (3,10)]
+
+tree = Tree(edges, node_values)
+tree.build_from_edges()
+
+assert tree.root.value == 'e' and tree.root.index == 4
+
+children = set(tree.root.children)
+
+grandchildren = set([])
+for child in children:
+    grandchildren = grandchildren.union(set(child.children))
+
+great_grandchildren = set([])
+for grandchild in grandchildren:
+    great_grandchildren = great_grandchildren.union(set(grandchild.children))
+
+great_great_grandchildren = set([])
+for great_grandchild in great_grandchildren:
+    great_great_grandchildren = great_great_grandchildren.union(set(great_grandchild.children))
+
+assert {node.index for node in children} == {0, 8, 6}
+assert {node.value for node in children} == {'a', 'i', 'g'}
+
+assert {node.index for node in grandchildren} == {2,3}
+assert {node.value for node in grandchildren} == {'c','d'}
+
+assert {node.index for node in great_grandchildren} == {1,9,5,10}
+assert {node.value for node in great_grandchildren} == {'b','j','f','k'}
+
+assert {node.index for node in great_great_grandchildren} == {7}
+assert {node.value for node in great_great_grandchildren} == {'h'}
+
+
+node_values = ['a', 'b', 'a', 'a', 'a', 'b', 'a', 'b', 'a', 'b', 'b']
+
+tree = Tree(edges, node_values)
+tree.build_from_edges()
+
+assert tree.root.value == 'a' and tree.root.index == 4
+
+children = set(tree.root.children)
+
+grandchildren = set([])
+for child in children:
+    grandchildren = grandchildren.union(set(child.children))
+
+great_grandchildren = set([])
+for grandchild in grandchildren:
+    great_grandchildren = great_grandchildren.union(set(grandchild.children))
+
+great_great_grandchildren = set([])
+for great_grandchild in great_grandchildren:
+    great_great_grandchildren = great_great_grandchildren.union(set(great_grandchild.children))
+
+assert {node.index for node in children} == {0, 8, 6}
+assert {node.value for node in children} == {'a','a','a'}
+
+assert {node.index for node in grandchildren} == {2,3}
+assert {node.value for node in grandchildren} == {'a','a'}
+
+assert {node.index for node in great_grandchildren} == {1,9,5,10}
+assert {node.value for node in great_grandchildren} == {'b','b','b','b'}
+
+assert {node.index for node in great_great_grandchildren} == {7}
+assert {node.value for node in great_great_grandchildren} == {'b'}
